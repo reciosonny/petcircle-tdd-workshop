8a45c8e98d86091f5d9716fa583a359a8e3e90df
Sonny Recio <reciosonny@gmail.com>
2023-09-25 21:15:23 +0800

    UI counter tests: initial tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,57 +5,25 @@ import userEvent from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
 
 test('should show the initial increment value', () => {  
-  const element = render(<App initialValue={777} />);
-
-  expect(element.getByTestId("count")).toHaveTextContent("777");
+  
 });
 
 test('should default to 0 when initial value is not supplied', () => {  
-  const element = render(<App />);
-
-  expect(element.getByTestId("count")).toHaveTextContent("0");
+  
 });
 
 test("should increment value when + button is pressed", async () => {
-  const element = render(<App initialValue={1} />);
-  
-  await act(async () => {
-    const btnAdd = element.getByText("+");
-    userEvent.click(btnAdd);
-  });
 
-  expect(element.getByTestId("count")).toHaveTextContent("2");
 });
 
 test("should decrement value when - button is pressed", async () => {
-  const element = render(<App initialValue={1} />);
-  
-  await act(async () => {
-    const btnAdd = element.getByText("-");
-    userEvent.click(btnAdd);
-  });
 
-  expect(element.getByTestId("count")).toHaveTextContent("0");
 });
 
 test("should reset value to 0 when reset button is pressed", async() => {
-  const element = render(<App initialValue={7777} />);
-  
-  await act(async () => {
-    const btnAdd = element.getByText("Reset");
-    userEvent.click(btnAdd);
-  });
 
-  expect(element.getByTestId("count")).toHaveTextContent("0");
 });
 
 test("should no longer decrement to negative values when value is already 0", async () => {
-  const element = render(<App initialValue={0} />);
-  
-  await act(async () => {
-    const btnAdd = element.getByText("-");
-    userEvent.click(btnAdd);
-  });
 
-  expect(element.getByTestId("count")).toHaveTextContent("0");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,11 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App({ initialValue }: any) {
-    const [val, setVal] = useState(initialValue ?? 0);
 
     return (
         <div className="App">
             <h1>My counter</h1>
-            <div>
-              <span data-testid="count" style={{ fontSize: 32 }}>{val}</span>
-            </div>
-            <button onClick={() => setVal((prevVal: number) => prevVal + 1)}>
-                +
-            </button>
-            <button
-                onClick={() =>
-                    val > 0 && setVal((prevVal: number) => prevVal - 1)
-                }
-            >
-                -
-            </button>
-            <button onClick={() => setVal(0)}>Reset</button>
+            {/* TODO: Your implementation here */}
         </div>
     );
 }
